cdf165a4d93b2e727b28800282c4b8164fdfbaf6
Maksim Lisak2 <maksim-lisak@mail.ru>
2023-09-27 15:15:36 +0300

    get

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const port = 3000
 
 
 app.get('/init', async (req, res) =>  {
-    console.log("req", req.headers)
     const api_Statistics = req.headers.api_statistics
     //записываем в БД
     //получаем нашего пользователя
@@ -15,39 +14,57 @@ app.get('/init', async (req, res) =>  {
         const isUserCreate = await UserModel.findOne({ where: { api_Statistics: `${api_Statistics}` } })
         if(isUserCreate) {
             //уже есть такой пользователь, значит просто обновляем ему последнее время
+            isUserCreate.lastDate = new Date()
+            await isUserCreate.save()
         } else {
             //такого пользователя нет, значит создаём его
             UserModel.create({
                 startDate: new Date(),
                 lastDate: new Date(),
                 api_Standart: req.headers.api_standart || '',
-                api_Statistics: req.headers.api_statistics || '',
+                api_Statistics: api_Statistics || '',
                 api_Advert: req.headers.api_advert || '',
             }).catch(err=>console.error("Ошибка создания пользователя",err))
         }
-    } catch(err) {
-        console.error("Ошибка поиска пользователя",err)
-    } finally {
         res.send('SUCCESS!')
+    } catch(err) {
+        console.error("Ошибка поиска пользователя или изменения lastDate у него",err)
+        res.send('ERROR!')
     }
 })
 
-app.get('/init1', async (req, res) =>  {
-    //открываем БД
+app.get('/startBD', async (req, res) =>  {
+    //запускаем БД
     try {
         await sequelize.authenticate()
-        await sequelize.sync({force: true})
+        await sequelize.sync(/* {force: true} */)
         console.log("подключенно к БД")
+        res.send('SUCCESS Start BD!')
     } catch (e) {
-        console.error('error', e)
-        console.error("подключение к БД сломалось")
-    } finally {
-        res.send('SUCCESS!')
+        console.error('подключение к БД сломалось', e)
+        res.send('ERROR!')
     }
 })
+
+// app.get('/stopBD', async (req, res) => {
+//     //останавливаем БД
+//     try {
+//         await sequelize.authenticate()
+//         await sequelize.sync(/* {force: true} */)
+//         console.log("подключенно к БД")
+//     } catch (e) {
+//         console.error('error', e)
+//         console.error("подключение к БД сломалось")
+//     } finally {
+//         res.send('SUCCESS!')
+//     }
+// })
 
 app.get('/getCount', (req, res) => {
     //Получаем из бд
+    const data = UserModel.findAll()
+    console.log("data", data)
+    res.send(data)
 })
 
 
