36efd236e40fc598bfd79d173c97ddc853bcb43c
Maksim Lisak2 <maksim-lisak@mail.ru>
2023-09-27 14:28:45 +0300

    success

--- index.js
+++ index.js
@@ -11,21 +11,25 @@ app.get('/init', async (req, res) =>  {
     const api_Statistics = req.headers.api_statistics
     //записываем в БД
     //получаем нашего пользователя
-    const isUserCreate = await UserModel.findOne({ where: { api_Statistics: `${api_Statistics}` } })
-    if(isUserCreate) {
-        //уже есть такой пользователь, значит просто обновляем ему последнее время
-    } else {
-        //такого пользователя нет, значит создаём его
-        UserModel.create({
-            startDate: new Date(),
-            lastDate: new Date(),
-            api_Standart: req.headers.api_standart || '',
-            api_Statistics: req.headers.api_statistics || '',
-            api_Advert: req.headers.api_advert || '',
-        }).catch(err=>console.error("Ошибка создания пользователя",err))
+    try{
+        const isUserCreate = await UserModel.findOne({ where: { api_Statistics: `${api_Statistics}` } })
+        if(isUserCreate) {
+            //уже есть такой пользователь, значит просто обновляем ему последнее время
+        } else {
+            //такого пользователя нет, значит создаём его
+            UserModel.create({
+                startDate: new Date(),
+                lastDate: new Date(),
+                api_Standart: req.headers.api_standart || '',
+                api_Statistics: req.headers.api_statistics || '',
+                api_Advert: req.headers.api_advert || '',
+            }).catch(err=>console.error("Ошибка создания пользователя",err))
+        }
+    } catch(err) {
+        console.error("Ошибка поиска пользователя",err)
+    } finally {
+        res.send('SUCCESS!')
     }
-
-
 })
 
 app.get('/init1', async (req, res) =>  {
@@ -37,8 +41,9 @@ app.get('/init1', async (req, res) =>  {
     } catch (e) {
         console.error('error', e)
         console.error("подключение к БД сломалось")
+    } finally {
+        res.send('SUCCESS!')
     }
-
 })
 
 app.get('/getCount', (req, res) => {
